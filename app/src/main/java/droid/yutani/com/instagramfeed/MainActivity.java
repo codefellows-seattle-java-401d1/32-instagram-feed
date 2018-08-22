6ecc1871a9b27878fe66ded82cdf8893eb38edf1
brandon-fenty <brandonfenty@gmail.com>
2018-08-22 12:08:49 -0700

    app displaying placeholder

--- app/src/main/java/droid/yutani/com/instagramfeed/MainActivity.java
+++ app/src/main/java/droid/yutani/com/instagramfeed/MainActivity.java
@@ -1,13 +1,30 @@
 package droid.yutani.com.instagramfeed;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.image_view)
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        loadImgs();
     }
+
+    public void loadImgs() {
+        Drawable drawable = getResources().getDrawable(R.drawable.placeholder);
+        mImageView.setImageDrawable(drawable);
+    }
+
+
 }

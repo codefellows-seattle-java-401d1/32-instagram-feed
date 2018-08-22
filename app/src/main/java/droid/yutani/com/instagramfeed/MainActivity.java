93012faa212b729c18fd321863f2b6ce6ead6d0e
brandon-fenty <brandonfenty@gmail.com>
2018-08-22 12:16:28 -0700

    images loading from ion

--- app/src/main/java/droid/yutani/com/instagramfeed/MainActivity.java
+++ app/src/main/java/droid/yutani/com/instagramfeed/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.koushikdutta.ion.Ion;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadImgs() {
-        Drawable drawable = getResources().getDrawable(R.drawable.placeholder);
-        mImageView.setImageDrawable(drawable);
+        String url = "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F43d5eab0-3659-11e7-a950-1fd679d420f6.jpg?crop=5211%2C2931%2C206%2C76&resize=685";
+        Ion.with(mImageView)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error_gif)
+                .load(url);
     }
 
 

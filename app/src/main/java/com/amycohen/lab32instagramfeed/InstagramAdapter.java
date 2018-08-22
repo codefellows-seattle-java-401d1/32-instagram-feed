58f42ce40fa97a05c745f380ca5af0352e90ea5f
Amy Cohen <anbcohen@gmail.com>
2018-08-22 10:35:08 -0700

    added list and had it be populated by the posts

--- app/src/main/java/com/amycohen/lab32instagramfeed/InstagramAdapter.java
+++ app/src/main/java/com/amycohen/lab32instagramfeed/InstagramAdapter.java
@@ -1,4 +1,44 @@
 package com.amycohen.lab32instagramfeed;
 
-class InstagramAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class InstagramAdapter extends RecyclerView.Adapter<InstagramAdapter.MyViewHolder>{
+
+    public List<InstagramPost> mPosts;
+    public InstagramAdapter(List<InstagramPost> posts) {
+
+        mPosts = posts;
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }

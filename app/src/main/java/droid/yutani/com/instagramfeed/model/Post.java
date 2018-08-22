6f26a435871d256d90097c76be905fbc02835059
brandon-fenty <brandonfenty@gmail.com>
2018-08-22 12:49:49 -0700

    adds model

--- app/src/main/java/droid/yutani/com/instagramfeed/model/Post.java
+++ app/src/main/java/droid/yutani/com/instagramfeed/model/Post.java
@@ -1,4 +1,15 @@
 package droid.yutani.com.instagramfeed.model;
 
 public class Post {
+    public String imageUrl;
+    public String owner;
+    public String location;
+    public String desc;
+
+    public Post(String imageUrl, String owner, String location, String desc) {
+        this.imageUrl = imageUrl;
+        this.owner = owner;
+        this.location = location;
+        this.desc = desc;
+    }
 }

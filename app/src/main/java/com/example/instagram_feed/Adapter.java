f8152810663ebc5aa9f39e97a6c927d972e1e61b
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-08-22 15:15:51 -0700

    Problems with adapter.

--- app/src/main/java/com/example/instagram_feed/Adapter.java
+++ app/src/main/java/com/example/instagram_feed/Adapter.java
@@ -4,6 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
+
+import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +27,41 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = 
+        View view = inflater.inflate(R.layout.activity_instagram, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        InstagramPost post = mFeed.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFeed.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public ImageView mImage;
+        public TextView mAuthor;
+        private TextView mDescription;
+
+        private InstagramPost mPost;
+    }
+
+    public MyViewHolder(View view) {
+        super(view);
+        mView = view;
+
+        mImage = view.findViewById(R.id.pictures);
+        mAuthor = view.findViewById(R.id.author);
+        mDescription = view.findViewById(R.id.description);
+
+        Ion.with(mImage).placeholder(R.drawable.loading).load(mPost.imageUrl);
     }
 }
--- app/src/main/java/com/example/instagram_feed/MainActivity.java
+++ app/src/main/java/com/example/instagram_feed/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Adapter;
+import android.support.v7.widget.RecyclerView;
 import android.widget.LinearLayout;
 
 
